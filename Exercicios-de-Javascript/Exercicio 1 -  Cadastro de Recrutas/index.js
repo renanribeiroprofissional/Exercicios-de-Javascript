b990e8dff1c7b7549d4530f6c14190fb125511d4
renanribeiroprofissional <renanprogramadorprofisional@gmail.com>
2024-11-26 23:09:56 -0300

    Removendo repositório interno

---
+++ Exercicios-de-Javascript/Exercicio 1 -  Cadastro de Recrutas/index.js
@@ -0,0 +1,26 @@
+/*Cadastro de Recrutas
+
+Escreva um programa em javascript que permita salvar informações de um recruta. As informações a serem salvas são:
+
+o primeiro nome
+
+o sobrenome
+
+o campo de estudo
+
+o ano de nascimento
+
+Depois o programa deve exibir o nome completo do recruta, seu campo de estudo e sua idade (apenas baseada no ano de nascimento)*/
+
+
+let nome = prompt("Digite o seu nome:")
+
+let sobrenome =prompt("Digite seu sobrenome")
+
+let estudo = prompt("Qual seu curso ?")
+
+let = idade = parseFloat(prompt("Digite a seu o ano de nascimento:"))
+
+let idadeconvert = (2024 - idade)
+
+alert("Seu nome completo é " +nome +" "+ sobrenome + ", você estuda " + estudo + " e você tem " + idadeconvert + " anos.")
